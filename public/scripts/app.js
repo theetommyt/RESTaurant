053799efc61b4e1ed325df629bd7ba6a49c178e5
Tom Taylor <tom.taylor@outlook.com>
2015-04-28 11:51:05 -0500

    created food backbone mvc files

---
+++ public/scripts/app.js
@@ -0,0 +1,40 @@
+var app = app || {};
+
+$(document).ready(function(){
+
+
+  app.foods = new app.FoodCollection({
+    model: app.FoodModel
+  })
+
+  app.parties = new app.PartyCollection({
+    model: app.PartyModel
+  })
+
+  app.orders = new app.OrderCollection({
+    model: app.OrderModel
+  })
+
+  app.foodListPainter = new app.GeneralListView({
+    modelView: app.FoodView,
+    collection: app.foods,
+    el: $('.button'),
+  });
+
+  app.partyListPainter = new app.GeneralListView({
+    modelView: app.PartyView,
+    collection: app.parties,
+    el: $('#party-list'),
+  });
+
+  app.orderListPainter = new app.GeneralListView({
+    modelView: app.OrderView,
+    collection: app.orders,
+    el: $('#order-list'),
+  });
+
+  app.parties.fetch();
+  app.foods.fetch();
+  app.orders.fetch();
+
+});
