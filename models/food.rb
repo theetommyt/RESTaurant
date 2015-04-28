19dcb4bcd0c60d777bf39ea6aa1c31977f2f38a8
Tom Taylor <tom.taylor@outlook.com>
2015-04-28 10:13:42 -0500

    debugging

--- models/food.rb
+++ models/food.rb
@@ -3,20 +3,6 @@ class Food < ActiveRecord::Base
   belongs_to(:orders)
   has_many(:parties, :through => :orders)
 
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def price
-    @price
-  end
-
-  def price=(new_price)
-    @price = new_price
-  end
+  
 
 end
