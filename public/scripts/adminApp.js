62722e9b26cda9774309cc5453d233739a46d300
Tom Taylor <tom.taylor@outlook.com>
2015-04-29 19:45:11 -0500

    foods can be added to db from auth page

--- public/scripts/adminApp.js
+++ public/scripts/adminApp.js
@@ -2,26 +2,7 @@ var app = app || {};
 
 app.foods = new FoodCollection({
   model: app.FoodModel,
-  events:{
-    'click #new_food': 'new_food'
-  },
-  intialize: function(){
 
-    var foodSubmit = $('#new_food');
-      foodSubmit.submit(function (e) {
-          $.ajax({
-              type: post,
-              url: '/api/foods',
-              data: $('#new_food').serialize() ,
-              success: function (data) {
-                  alert('New Food Created');
-              }
-              dataType: JSON
-          });
-
-          e.preventDefault();
-      });
-  }
 
 });
 
